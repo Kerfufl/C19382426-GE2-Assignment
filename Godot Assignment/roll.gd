34dc70e7af3292cfbcf58c7a82adb76d226ba93d
Kerfufl <c19382426@mytudublin.ie>
2023-04-28 20:46:05 +0100

    Raycasting testing
    Getting a feel for raycasting in the environment

--- Godot Assignment/roll.gd
+++ Godot Assignment/roll.gd
@@ -1,6 +1,7 @@
 extends RigidBody
 
 onready var rg = get_node(".")
+export var speed = 5
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,13 +9,19 @@ onready var rg = get_node(".")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var space_state = get_world().direct_space_state
+	var test = space_state.intersect_ray(rg.global_transform.origin,Vector3(0,-1,0))
+	print(test)
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	var bruh =rg.linear_velocity
-	rg.add_central_force(bruh)
-	DebugDraw.draw_line(rg.global_transform.origin, bruh, Color.chartreuse)
+	var space_state = get_world().direct_space_state
+	var down = space_state.intersect_ray(rg.global_transform.origin,Vector3(0,-1,0))
+	rg.add_central_force(Vector3(0,0,100) * - speed*delta)
+	#print(down)
+	DebugDraw.draw_line(rg.transform.origin, Vector3(0,0,100)*-speed, Color.chartreuse)
+	DebugDraw.draw_line(rg.transform.origin, Vector3(rg.transform.origin.x,rg.transform.origin.y-1,rg.transform.origin.z), Color.beige)
 	#pass

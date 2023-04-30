fde8dfbbfb8087b45b8e57462507b5bfa2220e14
Kerfufl <c19382426@mytudublin.ie>
2023-05-01 00:52:12 +0100

    School Testing
    Implemented school behaviours, modifying as necessary to fit the scene

--- Godot Assignment/root.gd
+++ Godot Assignment/root.gd
@@ -21,24 +21,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-#	draw_gizmos()
-#	var g = _create_graph("FPS", true, false, DebugDraw.BlockPosition_LeftTop if Engine.editor_hint else DebugDraw.BlockPosition_RightTop, DebugDraw.GraphTextFlags_Current | DebugDraw.GraphTextFlags_Avarage | DebugDraw.GraphTextFlags_Max | DebugDraw.GraphTextFlags_Min, Vector2(200, 80), custom_font)
-#	g.buffer_size = 300
+	draw_gizmos()
+	var g = _create_graph("FPS", true, false, DebugDraw.BlockPosition_LeftTop if Engine.editor_hint else DebugDraw.BlockPosition_RightTop, DebugDraw.GraphTextFlags_Current | DebugDraw.GraphTextFlags_Avarage | DebugDraw.GraphTextFlags_Max | DebugDraw.GraphTextFlags_Min, Vector2(200, 80), custom_font)
+	g.buffer_size = 300
 #
-##func _create_graph(title, is_fps, show_title, pos, flags, size = Vector2(256, 60), font = null) -> DebugDraw.GraphParameters:
-##	var graph = DebugDraw.get_graph_config(title)
-##	if !graph:
-##		if is_fps:
-##			graph = DebugDraw.create_fps_graph(title)
-##		else:
-##			graph = DebugDraw.create_graph(title)
-##
-##		if graph:
-##			graph.size = size
-##			graph.buffer_size = 50
-##			graph.position = pos
-##			graph.show_title = show_title
-##			graph.show_text_flags = flags
-##			graph.custom_font = font			
-##			graph.frame_time_mode = false
-##	return graph
+func _create_graph(title, is_fps, show_title, pos, flags, size = Vector2(256, 60), font = null) -> DebugDraw.GraphParameters:
+	var graph = DebugDraw.get_graph_config(title)
+	if !graph:
+		if is_fps:
+			graph = DebugDraw.create_fps_graph(title)
+		else:
+			graph = DebugDraw.create_graph(title)
+
+		if graph:
+			graph.size = size
+			graph.buffer_size = 50
+			graph.position = pos
+			graph.show_title = show_title
+			graph.show_text_flags = flags
+			graph.custom_font = font			
+			graph.frame_time_mode = false
+	return graph

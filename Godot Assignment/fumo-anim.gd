20e7801033df1728880c7d62426451eb93ab8698
Kerfufl <c19382426@mytudublin.ie>
2023-05-01 13:47:50 +0100

    More animation set up
    Linked animation on fairy to play on loop

--- Godot Assignment/fumo-anim.gd
+++ Godot Assignment/fumo-anim.gd
@@ -4,15 +4,16 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var anim =get_child(1)
+var anim = get_child(1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	anim.play("Take 001")
+	anim.play("CirnoDefRig|Vibing")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if anim.current_animation != "Take 001":
-		anim.play("Take 001")
+	if anim.current_animation != "CirnoDefRig|Vibing":
+		anim.play("CirnoDefRig|Vibing")
+	pass
